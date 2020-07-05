a374555b93746380a6e7c614ca62b34c05d7f09f
ChristianGonzalez7 <christiangonzalez1994@gmail.com>
2020-07-05 17:14:20 +0200

    conection mysql and heroku

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'christian7',
-    database: 'aleatory_meals'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bc2259851e7a0a',
+    password: '271c0020',
+    database: 'heroku_b0eac2baed98b33'
 });
 connection.connect();
 
